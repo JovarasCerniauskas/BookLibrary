6708a9a69c9e452c1e2d3da062f7979ed390ad94
Jovaras Cerniauskas <38849000+hydronex@users.noreply.github.com>
2021-04-06 14:01:12 +0300

    Add files via upload
    Comminting all files, including json file with all of the books - "json_data"

---
+++ BookLibrary/BookLibrary/BookReadWriteController.cs
@@ -0,0 +1,44 @@
+﻿using BookLibrary.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary
+{
+    class BookReadWriteController
+    {
+        public List<Book> books { get; set; }
+
+        public BookReadWriteController()
+        {
+            books = LoadData();
+        }
+
+        private List<Book> LoadData()
+        {
+                using (StreamReader r = new StreamReader("Json_data.json"))
+                {
+                    string json = r.ReadToEnd();
+                    return JsonConvert.DeserializeObject<List<Book>>(json);
+                   
+                }
+
+            return null;
+        }
+
+        public void SaveData()
+        {
+            var json = JsonConvert.SerializeObject(books, Formatting.Indented);
+            using (StreamWriter w = new StreamWriter("Json_data.json"))
+            {
+                w.Write(json);
+            }
+
+            //save books
+        }
+    }
+}
---
+++ BookLibrary/BookLibrary/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Models
+{
+    class Book
+    {
+        public string ISBN { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Language { get; set; }
+        public string Category { get; set; }
+        public bool Istaken { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public string TakenContactName { get; set; }
+        public DateTime? TakenDate { get; set; }
+
+    }
+}
